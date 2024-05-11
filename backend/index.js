64037520e75413b5ee058b7ecc67c93ea628fa2e
Tim Lin <htlin2@gmail.com>
2024-05-11 16:14:55 +0800

    backend

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,21 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 app.use(express.json());
 
+app.get("/doctors", async (req, res) => {
+  const query = await pool.query("SELECT * FROM doctors ORDER BY id");
+  res.json(query.rows);
+});
+
+app.get("/appointments", async (req, res) => {
+  const query = await pool.query(`
+      SELECT * from appointments
+      JOIN patients ON appointments."patientId" = patients.id
+      JOIN doctors ON appointments."doctorId" = doctors.id
+      ORDER BY appointments.id
+    `);
+  res.json(query.rows);
+});
+
 app.get("/", async (req, res) => {
   res.send("Hello World!");
 });
--- backend/init.sql
+++ backend/init.sql
@@ -6,3 +6,38 @@ BEGIN
 END $$;
 
 \c my_app_dev;
+
+CREATE TABLE IF NOT EXISTS doctors (
+    "id" SERIAL PRIMARY KEY,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS patients (
+    "id" SERIAL PRIMARY KEY,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS appointments (
+    "id" SERIAL PRIMARY KEY,
+    "patientId" INT NOT NULL,
+    "doctorId" INT NOT NULL,
+    "time" TEXT NOT NULL,
+    "kind" TEXT NOT NULL,
+    FOREIGN KEY ("doctorId") REFERENCES "doctors"("id"),
+    FOREIGN KEY ("patientId") REFERENCES "patients"("id")
+);
+
+INSERT INTO patients ("firstName", "lastName")
+VALUES ('John', 'Doe'),
+       ('Jane', 'Smith');
+
+INSERT INTO doctors ("firstName", "lastName", "email")
+VALUES ('Michael', 'Johnson', 'michael@example.com'),
+       ('Sarah', 'Williams', 'sarah@example.com');
+
+INSERT INTO appointments ("patientId", "doctorId", "time", "kind")
+VALUES (1, 1, '10:00', 'Follow-up'),
+       (2, 2, '14:30', 'New Patient');
